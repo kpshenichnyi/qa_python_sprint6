ac5ecbdc78409ac2914bf9fd69b4ba1fd09c439f
Konstantin Pshenichnyi <kpshenichnyi@yandex.ru>
2024-05-30 05:25:03 +0300

    "Можно лучше"-2: перенос описания пар вопрос-ответ из параметров теста test_scooter_faq.py в отдельный словарь в файле data.py

--- tests/test_scooter_faq.py
+++ tests/test_scooter_faq.py
@@ -5,29 +5,37 @@ import allure
 from pages.base_page import BasePage
 from pages.main_page_scooter import MainPageScooter, MainPageFAQSection
 from locators.locators_main_page import LocatorsFAQ
-
+from data import TestDataFAQ
 
 from conftest import driver
 
 class TestFaqScooterRental:
 
     @pytest.mark.parametrize("test_faq_id, actual_question, actual_answer, expected_question, expected_answer", [
-                            (MainPageFAQSection.faq_click_question_id_0, MainPageFAQSection.get_text_question_for_faq_0, MainPageFAQSection.get_text_answer_for_faq_0,
-                                "Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."),
-                            (MainPageFAQSection.faq_click_question_id_1, MainPageFAQSection.get_text_question_for_faq_1, MainPageFAQSection.get_text_answer_for_faq_1,
-                                "Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."),
-                            (MainPageFAQSection.faq_click_question_id_2, MainPageFAQSection.get_text_question_for_faq_2, MainPageFAQSection.get_text_answer_for_faq_2,
-                                "Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."),
-                            (MainPageFAQSection.faq_click_question_id_3, MainPageFAQSection.get_text_question_for_faq_3, MainPageFAQSection.get_text_answer_for_faq_3,
-                                "Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."),
-                            (MainPageFAQSection.faq_click_question_id_4, MainPageFAQSection.get_text_question_for_faq_4, MainPageFAQSection.get_text_answer_for_faq_4,
-                              "Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."),
-                            (MainPageFAQSection.faq_click_question_id_5, MainPageFAQSection.get_text_question_for_faq_5, MainPageFAQSection.get_text_answer_for_faq_5,
-                              "Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."),
-                            (MainPageFAQSection.faq_click_question_id_6, MainPageFAQSection.get_text_question_for_faq_6, MainPageFAQSection.get_text_answer_for_faq_6,
-                              "Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."),
-                            (MainPageFAQSection.faq_click_question_id_7, MainPageFAQSection.get_text_question_for_faq_7, MainPageFAQSection.get_text_answer_for_faq_7,
-                              "Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области.")
+                            (MainPageFAQSection.faq_click_question_id_0,
+                                MainPageFAQSection.get_text_question_for_faq_0, MainPageFAQSection.get_text_answer_for_faq_0,
+                                    TestDataFAQ.dict_expected_faq[0][0], TestDataFAQ.dict_expected_faq[0][1]),
+                            (MainPageFAQSection.faq_click_question_id_1,
+                                MainPageFAQSection.get_text_question_for_faq_1, MainPageFAQSection.get_text_answer_for_faq_1,
+                                    TestDataFAQ.dict_expected_faq[1][0], TestDataFAQ.dict_expected_faq[1][1]),
+                            (MainPageFAQSection.faq_click_question_id_2,
+                                MainPageFAQSection.get_text_question_for_faq_2, MainPageFAQSection.get_text_answer_for_faq_2,
+                                    TestDataFAQ.dict_expected_faq[2][0], TestDataFAQ.dict_expected_faq[2][1]),
+                            (MainPageFAQSection.faq_click_question_id_3,
+                                MainPageFAQSection.get_text_question_for_faq_3, MainPageFAQSection.get_text_answer_for_faq_3,
+                                    TestDataFAQ.dict_expected_faq[3][0], TestDataFAQ.dict_expected_faq[3][1]),
+                            (MainPageFAQSection.faq_click_question_id_4,
+                                MainPageFAQSection.get_text_question_for_faq_4, MainPageFAQSection.get_text_answer_for_faq_4,
+                                    TestDataFAQ.dict_expected_faq[4][0], TestDataFAQ.dict_expected_faq[4][1]),
+                            (MainPageFAQSection.faq_click_question_id_5,
+                                MainPageFAQSection.get_text_question_for_faq_5, MainPageFAQSection.get_text_answer_for_faq_5,
+                                    TestDataFAQ.dict_expected_faq[5][0], TestDataFAQ.dict_expected_faq[5][1]),
+                            (MainPageFAQSection.faq_click_question_id_6,
+                                MainPageFAQSection.get_text_question_for_faq_6, MainPageFAQSection.get_text_answer_for_faq_6,
+                                    TestDataFAQ.dict_expected_faq[6][0], TestDataFAQ.dict_expected_faq[6][1]),
+                            (MainPageFAQSection.faq_click_question_id_7,
+                                MainPageFAQSection.get_text_question_for_faq_7, MainPageFAQSection.get_text_answer_for_faq_7,
+                                    TestDataFAQ.dict_expected_faq[7][0], TestDataFAQ.dict_expected_faq[7][1])
                             ])
     def test_click_on_all_section_questions_answers(self, driver, test_faq_id, actual_question, actual_answer, expected_question, expected_answer):
         test_page = MainPageFAQSection(driver)
